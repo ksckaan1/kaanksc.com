a122cce35f552ea25c441e23cc369cb0e17bcfc7
Kaan Kuscu <me@kaanksc.com>
2022-08-20 01:53:47 +0300

    removed repo images

--- src/components/RepoCard.svelte
+++ src/components/RepoCard.svelte
@@ -6,10 +6,12 @@
 </script>
 <a href={repo.html_url} target="_blank">
     <div class="bg-teal-700 rounded overflow-hidden group">
-        <img src={"https://opengraph.githubassets.com/1/"+repo.full_name} alt="cover"
-             class="w-full group-hover:scale-105 transition aspect-video" in:fly={{duration:200, delay: 100 * index, y: -100}}>
         <div class="w-full bg-teal-900 px-3 py-1 text-center">{repo.name}</div>
         <div class="p-3 text-sm overflow-hidden text-center">{repo.description || "Açıklama yok.."}</div>
+        <div class="flex justify-center gap-x-2 mb-3">
+            <div class="flex gap-x-1"><img class="w-3" src="/icons/Star.svg" alt=""><span class="text-sm">{repo.stargazers_count}</span></div>
+            <div class="flex gap-x-1"><img class="w-3" src="/icons/Fork.svg" alt=""><span class="text-sm">{repo.forks}</span></div>
+        </div>
         <div class="w-full bg-teal-900 px-3 py-1 flex">
             <button class="flex-1">Browse</button>
 
