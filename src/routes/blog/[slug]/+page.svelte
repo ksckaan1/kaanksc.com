e0448c5b07a814e78453dd3d5f4e40036632d519
Kaan Kuscu <me@kaanksc.com>
2023-09-22 19:10:55 +0300

    fix transitions when navigating to post from other post

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -6,6 +6,13 @@
 
     export let data;
     let path = $page.url.pathname.split("/");
+    $: slug = path[path.length - 1]
+
+    $: {
+        if ($page.url) {
+            path = [...$page.url.pathname.split("/")]
+        }
+    }
 </script>
 
 <svelte:head>
@@ -15,34 +22,39 @@
     <meta name="author" content="Kaan Kuscu"/>
 </svelte:head>
 
-<div
-        in:fly|global={{ duration: 200, delay: 200, y: 50 }}
-        out:fade|global={{ duration: 200 }}
-        class="wta flex flex-col my-5 mx-auto max-w-5xl lg:bg-black/20 lg:rounded-lg lg:border lg:border-white/20 p-5"
->
-    <div class="text-kYellow text-2xl sm:text-4xl font-bold">{data.meta.title}</div>
-    <div class="font-light text-xl mt-3 text-kWhite/50">
-        {formatDate(data.meta.date)}
-    </div>
-    <div class="flex gap-3 mt-3 flex-wrap">
-        {#each data.meta.tags as tag}
-            <span class="text-lg text-kWhite/90">#{tag}</span>
-        {/each}
-    </div>
-</div>
+{#key slug}
+    <div>
 
-<div
-        in:fly|global={{ duration: 200, delay: 400, y: 50 }}
-        out:fade|global={{ duration: 200 }}
-        class="bg-zinc-900 border-t-8 border-kWhite/20 pb-20 lg:pb-0"
->
-    <div class="prose prose-invert text-kWhite p-5 max-w-3xl w-full mx-auto">
-        <svelte:component this={data.content}/>
-        <hr>
-    </div>
+        <div
+                in:fly|global={{ duration: 200, delay: 200, y: 50 }}
+                out:fade|global={{ duration: 200 }}
+                class="wta flex flex-col my-5 mx-auto max-w-5xl lg:bg-black/20 lg:rounded-lg lg:border lg:border-white/20 p-5"
+        >
+            <div class="text-kYellow text-2xl sm:text-4xl font-bold">{data.meta.title}</div>
+            <div class="font-light text-xl mt-3 text-kWhite/50">
+                {formatDate(data.meta.date)}
+            </div>
+            <div class="flex gap-3 mt-3 flex-wrap">
+                {#each data.meta.tags as tag}
+                    <span class="text-lg text-kWhite/90">#{tag}</span>
+                {/each}
+            </div>
+        </div>
+
+        <div
+                in:fly|global={{ duration: 200, delay: 400, y: 50 }}
+                out:fade|global={{ duration: 200 }}
+                class="bg-zinc-900 border-t-8 border-kWhite/20 pb-20 lg:pb-0"
+        >
+            <div class="prose prose-invert text-kWhite p-5 max-w-3xl w-full mx-auto">
+                <svelte:component this={data.content}/>
+                <hr>
+            </div>
 
 
-    <div class="max-w-3xl w-full mx-auto p-5">
-        <Avatar/>
+            <div class="max-w-3xl w-full mx-auto p-5">
+                <Avatar/>
+            </div>
+        </div>
     </div>
-</div>
+{/key}
