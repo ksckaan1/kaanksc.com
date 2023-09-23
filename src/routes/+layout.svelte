f9f727000328bb554bb11e4ef614f1b12d397f92
Kaan Kuscu <me@kaanksc.com>
2023-09-23 19:48:51 +0300

    add "og:url" meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import "$lib/style/main.css";
     import Header from "$lib/components/Header.svelte";
+    import {page} from "$app/stores";
 </script>
 
 <svelte:head>
@@ -8,6 +9,7 @@
     <meta name="twitter:creator" content="@ksckaan1">
     <meta name="twitter:site" content="@ksckaan1">
     <meta property="og:site_name" content="Kaan Kuscu - Backend Developer">
+    <meta property="og:url" content={$page.url.href}>
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:widgets:new-embed-design" content="on">
 </svelte:head>
