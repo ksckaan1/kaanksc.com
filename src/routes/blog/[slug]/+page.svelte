10ea78aa8253eeef51abc7a1543aac7f5f2cc6f4
Kaan Kuscu <me@kaanksc.com>
2024-10-21 23:40:19 +0300

    update post page header design

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -37,59 +37,56 @@
   <meta property="twitter:image" content={createOGImageLink(data.meta.title)} />
 </svelte:head>
 
-<div class="max-w-5xl w-full px-5 mx-auto mt-5">
-  <div class="flex justify-between items-center text-primary">
+<!-- POST HEADER -->
+<div
+  class="w-full gridbg border-b border-white/20 py-5 text-primary"
+  use:ripple
+>
+  <div class="max-w-5xl w-full px-5 mx-auto">
     <a
       use:ripple
-      class="text-primary self-start rounded font-light text-4xl flex gap-x-2 items-center"
+      class="text-primary self-start rounded font-light text-4xl"
       href="/blog/"
     >
       <h1 class="section-title">YAZILAR</h1></a
     >
-    <a
-      href="/rss/"
-      class="p-3 rounded-full"
-      target="_blank"
-      use:ripple={{ center: true }}
-    >
-      <Icon icon="line-md:rss" width="32" height="32" />
-    </a>
-  </div>
-  <div
-    use:ripple
-    class="p-5 flex flex-col gap-3 border mt-5 gridbg border-white/20 text-primary rounded"
-  >
-    <h2 class="text-primary text-3xl font-light">{data.meta.title}</h2>
-    <p class="text-white/60 font-light">{formatDate(data.meta.date)}</p>
-    <div class="text-white/80 flex flex-wrap gap-2 font-light">
-      {#each data.meta.tags as tag}
-        <span class="text-lg">#{tag}</span>
-      {/each}
+    <div class="flex flex-col gap-3 py-5 text-primary rounded">
+      <h2 class="text-primary text-3xl font-light">{data.meta.title}</h2>
+      <p class="text-white/60 font-light">{formatDate(data.meta.date)}</p>
+      <div class="text-white/80 flex flex-wrap gap-2 font-light">
+        {#each data.meta.tags as tag}
+          <span class="text-lg">#{tag}</span>
+        {/each}
+      </div>
     </div>
   </div>
+</div>
 
-  <div class="prose prose-invert max-w-2xl w-full mx-auto mt-10 text-slate-300">
-    <!-- svelte-ignore svelte_component_deprecated -->
-    <svelte:component this={data.content} />
-  </div>
-  <div class="flex gap-5 flex-wrap max-w-5xl w-full mx-auto mt-10">
-    {#each authors as author}
-      <a
-        href={author?.link}
-        class="flex gap-x-5 text-primary border w-full border-white/20 p-5 rounded items-center"
-        target="_blank"
-        use:ripple
-      >
-        <img
-          src={author?.avatar_url}
-          class="w-20 rounded-full border border-white/20"
-          alt={author?.full_name}
-        />
-        <div>
-          <h3 class="text-primary text-xl">{author?.full_name}</h3>
-          <h3 class="text-white/60">{author?.title}</h3>
-        </div>
-      </a>
-    {/each}
-  </div>
+<!-- POST FIELD -->
+
+<div
+  class="prose prose-invert max-w-2xl w-full px-5 mx-auto mt-10 text-slate-300"
+>
+  <!-- svelte-ignore svelte_component_deprecated -->
+  <svelte:component this={data.content} />
+</div>
+<div class="flex gap-5 flex-wrap max-w-5xl w-full mx-auto mt-10">
+  {#each authors as author}
+    <a
+      href={author?.link}
+      class="flex gap-x-5 text-primary border w-full border-white/20 p-5 rounded items-center"
+      target="_blank"
+      use:ripple
+    >
+      <img
+        src={author?.avatar_url}
+        class="w-20 rounded-full border border-white/20"
+        alt={author?.full_name}
+      />
+      <div>
+        <h3 class="text-primary text-xl">{author?.full_name}</h3>
+        <h3 class="text-white/60">{author?.title}</h3>
+      </div>
+    </a>
+  {/each}
 </div>
