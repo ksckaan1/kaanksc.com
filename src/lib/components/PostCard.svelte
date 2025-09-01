aa7705b430d92a14d96bba215505a0543364e169
Kaan Kuscu <me@kaanksc.com>
2025-09-01 10:06:25 +0300

    add skeleton loading for blog posts

---
+++ src/lib/components/PostCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { ripple } from "svelte-ripple-action";
+    import { formatDate } from "../../utils/format";
+    import type { Post } from "$lib/models/types";
+
+    interface Props {
+        post: Post;
+    }
+
+    let { post }: Props = $props();
+</script>
+
+<a
+    href="/blog/{post.slug}"
+    use:ripple
+    class="p-3 border border-white/20 text-primary rounded gridbg"
+>
+    <h2 class="text-primary text-xl font-light">{post.title}</h2>
+    <p class="text-white/60 font-light">{formatDate(post.date)}</p>
+    <p class="text-white/80 font-light">
+        {post.tags.map((t) => "#" + t).join(" ")}
+    </p>
+</a>
---
+++ src/lib/components/SkeletonPostCard.svelte
@@ -0,0 +1,7 @@
+<div
+    class="border flex flex-col justify-start p-5 items-start bg-white/5 border-white/20 gap-3 text-primary rounded"
+>
+    <div class="rounded w-2/3 bg-white/20 h-5 animate-pulse"></div>
+    <div class="rounded w-1/3 bg-white/20 h-5 animate-pulse"></div>
+    <div class="rounded w-2/3 bg-white/20 h-5 animate-pulse"></div>
+</div>
