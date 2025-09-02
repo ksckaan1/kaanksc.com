ecbac41cf8e247c433736ec56864239fbe307d42
Kaan Kuscu <me@kaanksc.com>
2025-09-03 00:52:15 +0300

    randomize skeleton post card widths

--- src/lib/components/SkeletonPostCard.svelte
+++ src/lib/components/SkeletonPostCard.svelte
@@ -1,7 +1,20 @@
+<script lang="ts">
+    const randomPercent = () => 30 + Math.ceil(Math.random() * 70) + "%";
+</script>
+
 <div
     class="border flex flex-col justify-start p-5 items-start bg-white/5 border-white/20 gap-3 text-primary rounded"
 >
-    <div class="rounded w-2/3 bg-white/20 h-5 animate-pulse"></div>
-    <div class="rounded w-1/3 bg-white/20 h-5 animate-pulse"></div>
-    <div class="rounded w-2/3 bg-white/20 h-5 animate-pulse"></div>
+    <div
+        style="width: {randomPercent()}"
+        class="rounded bg-white/20 h-5 animate-pulse"
+    ></div>
+    <div
+        style="width: {randomPercent()}"
+        class="rounded bg-white/20 h-5 animate-pulse"
+    ></div>
+    <div
+        style="width: {randomPercent()}"
+        class="rounded bg-white/20 h-5 animate-pulse"
+    ></div>
 </div>
