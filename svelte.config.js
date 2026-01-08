dc541b85d4702709c01531382b5cd71a50b0d5ae
ksckaan1 <me@kaanksc.com>
2026-01-08 23:47:44 +0300

    update green tea gc post (remove pnpm workspace)

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,32 @@
 import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import remarkEmoji from "remark-emoji";
 import remarkHint from "remark-hint";
+import remarkMermaid from "remark-mermaid";
 import relativeImages from "mdsvex-relative-images";
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-    extensions: ['.svx'],
-    highlight: {
-        highlighter: (code, lang, args) => {
-            return `<Components.pre code={\`${escape(code)}\`} lang={\`${lang ?? "text"}\`} args={\`${args ?? ""}\`} />`;
-        },
-    },
-    remarkPlugins: [
-        remarkEmoji,
-        remarkHint,
-        relativeImages,
-    ],
-    layout: {
-        _: './src/layouts/mdsvex.svelte'
-    },
-}
+	extensions: [".svx"],
+	highlight: {
+		highlighter: (code, lang, args) => {
+			return `<Components.pre code={\`${escape(code)}\`} lang={\`${lang ?? "text"}\`} args={\`${args ?? ""}\`} />`;
+		},
+	},
+	remarkPlugins: [remarkMermaid, remarkEmoji, remarkHint, relativeImages],
+	layout: {
+		_: "./src/layouts/mdsvex.svelte",
+	},
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-    kit: {
-        adapter: adapter()
-    },
-    extensions: [".svelte", ".svx"]
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+	kit: {
+		adapter: adapter(),
+	},
+	extensions: [".svelte", ".svx"],
 };
 
 export default config;
