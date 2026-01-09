bb60e405bbaa923c71d8bcfb6a61779796c4fa24
ksckaan1 <me@kaanksc.com>
2026-01-09 22:15:41 +0300

    add disabled prop to TextButton

--- src/lib/components/TextButton.svelte
+++ src/lib/components/TextButton.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-  import type { Snippet } from "svelte";
-  import { ripple } from "svelte-ripple-action";
+	import type { Snippet } from "svelte";
+	import { ripple } from "svelte-ripple-action";
 
-  interface Props {
-    variant?: 'primary';
-    onclick?: () => void;
-    children?: Snippet;
-  }
+	interface Props {
+		variant?: "primary";
+		onclick?: () => void;
+		children?: Snippet;
+		disabled?: boolean;
+	}
 
-  let {
-    variant = 'primary',
-    onclick,
-    children
-  }: Props = $props();
+	let {
+		variant = "primary",
+		onclick,
+		children,
+		disabled = false,
+	}: Props = $props();
 </script>
-<button {onclick} use:ripple class="text-primary rounded px-4 py-2">
-{@render children?.()}
+
+<button
+	{onclick}
+	use:ripple
+	class="text-primary rounded px-4 py-2"
+	class:opacity-50={disabled}
+	{disabled}
+>
+	{@render children?.()}
 </button>
