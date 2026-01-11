28d0440bc1c36e07b4fdbcf7fd7c6e6c0e3b9409
ksckaan1 <me@kaanksc.com>
2026-01-11 07:19:33 +0300

    imprv gallery component

--- src/lib/components/interactives/Gallery.svelte
+++ src/lib/components/interactives/Gallery.svelte
@@ -19,6 +19,14 @@
 
 	let isMaximized = $state(false);
 
+	$effect(() => {
+		if (isMaximized) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "auto";
+		}
+	});
+
 	const listenArrowKeys = (ev: KeyboardEvent) => {
 		if (!isMaximized) return;
 
@@ -29,57 +37,113 @@
 		if (ev.key === "ArrowRight" && activeIndex < elems.length - 1) {
 			activeIndex++;
 		}
+
+		if (ev.key === "Escape") {
+			isMaximized = false;
+		}
 	};
 
 	onMount(() => {
 		window.addEventListener("keydown", listenArrowKeys);
 
-		return () => {
-			window.removeEventListener("keypress", listenArrowKeys);
-		};
+		return () => window.removeEventListener("keypress", listenArrowKeys);
 	});
 </script>
 
 <div
-	class={isMaximized
-		? "fixed top-0 left-0 w-screen h-screen p-5 bg-black/70 flex items-center justify-center z-50"
-		: ""}
-	onclick={(e) => {
-		if (e.target == e.currentTarget) isMaximized = false;
-	}}
+	class="
+	flex flex-col
+	border border-white/20 gap-0 bg-gray-900 overflow-hidden rounded"
 >
-	<div
-		class="flex flex-col border border-white/20 p-0 m-0 gap-0 bg-gray-900 overflow-hidden rounded
-		{isMaximized
-			? 'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-6xl min-h-[calc(100vh - 10rem)]'
-			: ''}"
-	>
-		<img
-			class="m-0 border-b border-white/20 cursor-pointer"
-			src={elems[activeIndex].img}
-			alt={elems[activeIndex].caption}
-			onclick={() => (isMaximized = true)}
-		/>
-		<div class="flex justify-between mt-3 items-center">
-			<TextButton onclick={() => activeIndex--} disabled={activeIndex === 0}>
-				<div class="flex gap-x-3">
-					<Icon icon="mdi:arrow-left" height="30" />
-					<span>Önceki</span>
-				</div>
-			</TextButton>
-			<span class="text-primary/50">
-				{activeIndex + 1} / {elems.length}
-			</span>
-			<TextButton
-				onclick={() => activeIndex++}
-				disabled={activeIndex === elems.length - 1}
-			>
-				<div class="flex gap-x-3">
-					<span>Sonraki</span>
-					<Icon icon="mdi:arrow-right" height="30" />
-				</div>
-			</TextButton>
-		</div>
-		<p class="m-0 p-5 pt-0">{@html marked(elems[activeIndex].caption)}</p>
+	<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<img
+		class="m-0 border-b border-white/20 cursor-pointer"
+		src={elems[activeIndex].img}
+		alt={elems[activeIndex].caption}
+		onclick={() => (isMaximized = true)}
+	/>
+	<div class="flex justify-between mt-3 items-center">
+		<TextButton onclick={() => activeIndex--} disabled={activeIndex === 0}>
+			<div class="flex gap-x-3">
+				<Icon icon="mdi:arrow-left" height="30" />
+				<span>Önceki</span>
+			</div>
+		</TextButton>
+		<span class="text-primary/50 text-sm">
+			{activeIndex + 1} / {elems.length}
+		</span>
+		<TextButton
+			onclick={() => activeIndex++}
+			disabled={activeIndex === elems.length - 1}
+		>
+			<div class="flex gap-x-3">
+				<span>Sonraki</span>
+				<Icon icon="mdi:arrow-right" height="30" />
+			</div>
+		</TextButton>
 	</div>
+	{#if elems[activeIndex].caption}
+		<p class="m-0 p-5 pt-0">{@html marked(elems[activeIndex].caption)}</p>
+	{/if}
 </div>
+
+{#if isMaximized}
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<!-- svelte-ignore a11y_no_static_element_interactions -->
+	<div
+		class="
+		fixed top-0 left-0 w-screen h-screen z-50
+		flex items-center justify-center
+		p-10 bg-black/70 cursor-pointer backdrop-blur-xl"
+		onclick={(e) => {
+			if (e.target == e.currentTarget) isMaximized = false;
+		}}
+	>
+		<div
+			class="
+			flex flex-col relative
+			border border-white/20 gap-0 bg-gray-900 rounded w-full max-w-6xl
+			cursor-auto
+			"
+		>
+			<button
+				class="absolute right-0 -top-12 bg-gray-900 rounded border border-white/20 px-2 py-1"
+				onclick={() => (isMaximized = false)}
+			>
+				Esc
+			</button>
+			<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<img
+				class="m-0 border-b border-white/20 rounded-t"
+				src={elems[activeIndex].img}
+				alt={elems[activeIndex].caption}
+				onclick={() => (isMaximized = true)}
+			/>
+			<div class="flex justify-between mt-3 items-center">
+				<TextButton onclick={() => activeIndex--} disabled={activeIndex === 0}>
+					<div class="flex gap-x-3">
+						<Icon icon="mdi:arrow-left" height="30" />
+						<span>Önceki</span>
+					</div>
+				</TextButton>
+				<span class="text-primary/50 text-sm">
+					{activeIndex + 1} / {elems.length}
+				</span>
+				<TextButton
+					onclick={() => activeIndex++}
+					disabled={activeIndex === elems.length - 1}
+				>
+					<div class="flex gap-x-3">
+						<span>Sonraki</span>
+						<Icon icon="mdi:arrow-right" height="30" />
+					</div>
+				</TextButton>
+			</div>
+			{#if elems[activeIndex].caption}
+				<p class="m-0 p-5 pt-0">{@html marked(elems[activeIndex].caption)}</p>
+			{/if}
+		</div>
+	</div>
+{/if}
